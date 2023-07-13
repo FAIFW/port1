3b478f6e11ed03e924d9c3ac68622fc3a98c7190
FA1F.W <fa1994f.w@gmail.com>
2023-07-13 14:26:41 +0300

    -header mobile is ready;

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+//burger-btn
+(function() {
+    const burgerBtn = document.querySelector('.burger');
+    const mobileNav = document.querySelector('.nav');
+    const body = document.querySelector('body');
+    
+    burgerBtn.addEventListener('click', function() {
+        //burger button animation:
+        burgerBtn.classList.toggle('burger_active');
+        //navigation move:
+        mobileNav.classList.toggle('nav_active');
+        //body is being hidden:
+        body.classList.toggle('stop-scroll');
+    });
+})();
+
+
+
